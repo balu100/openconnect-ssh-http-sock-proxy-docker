5ce20ac21bec2477a9054fa6a62fb6bccc30b06a
balu100 <ebalu1@hotmail.com>
2025-03-24 22:11:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,27 +12,21 @@ else
     echo "WARNING: No SSH_PUB_KEY provided. SSH login might fail."
 fi
 
-# Define log files
-OPENCONNECT_LOG="/var/log/openconnect.log"
-SSHD_LOG="/var/log/sshd.log"
-SOCKD_LOG="/var/log/sockd.log"
-TINYPROXY_LOG="/var/log/tinyproxy.log"
-
 # Start the SSH daemon
-/usr/sbin/sshd -D >> "$SSHD_LOG" >&2 &
+/usr/sbin/sshd -D >> /var/log/openconnect.log &
 
 # Check if necessary environment variables are set
 if [ -z "$VPN_SERVER" ] || [ -z "$VPN_USERNAME" ] || [ -z "$VPN_PASSWORD" ] || [ -z "$VPN_SERVERCERT" ]; then
-    echo "VPN_SERVER, VPN_USERNAME, VPN_PASSWORD, VPN_AUTHGROUP, and VPN_SERVERCERT environment variables must be set" >> "$OPENCONNECT_LOG" >&2 
+    echo "VPN_SERVER, VPN_USERNAME, VPN_PASSWORD, VPN_AUTHGROUP, and VPN_SERVERCERT environment variables must be set" >> /var/log/openconnect.log >&2 
     exit 1
 fi
 
 # Start OpenConnect and log output
 echo "Starting OpenConnect..."
-echo "$VPN_PASSWORD" | openconnect --user="$VPN_USERNAME" --passwd-on-stdin --authgroup="$VPN_AUTHGROUP" --servercert "$VPN_SERVERCERT" "$VPN_SERVER" >> "$OPENCONNECT_LOG" 2>&1 &
+echo "$VPN_PASSWORD" | openconnect --user="$VPN_USERNAME" --passwd-on-stdin --authgroup="$VPN_AUTHGROUP" --servercert "$VPN_SERVERCERT" "$VPN_SERVER" >> /var/log/openconnect.log 2>&1 &
 sleep 5
-/usr/bin/tinyproxy >> "$TINYPROXY_LOG" >&2 &
-/usr/sbin/sockd -D >> "$SOCKD_LOG" >&2 &
+/usr/bin/tinyproxy >> /var/log/openconnect.log &
+/usr/sbin/sockd -D >> /var/log/openconnect.log &
 
 
 GW=$(ip route | awk '/default/ {print $3}')
@@ -45,11 +39,19 @@ while [ -n "$(printenv KEEP_LOCAL_IP$i)" ]; do
 done
 
 # Monitor the OpenConnect log for a BYE packet and exit the container when detected
-tail -f "$OPENCONNECT_LOG" | while IFS= read -r line; do
+tail -f /var/log/openconnect.log | while IFS= read -r line; do
     echo "$line"
     if echo "$line" | grep -q "BYE"; then
         echo "BYE packet detected. Exiting container."
-        # Optionally terminate background processes
+        pkill openconnect
+        pkill sshd
+        pkill tinyproxy
+        pkill sockd
+        pkill tail
+        exit 0
+    fi
+    if echo "$line" | grep -q "shutting down"; then
+        echo "Dante down signal detected. Exiting container."
         pkill openconnect
         pkill sshd
         pkill tinyproxy
